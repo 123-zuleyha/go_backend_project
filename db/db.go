46578dd5d7096b750028a14c8ad29aa62e64c191
Züleyha <yetkinzuleyha94@gmail.com>
2024-08-15 11:46:14 +0300

    db.go added

--- db/db.go
+++ db/db.go
@@ -1,2 +1,50 @@
 package db
 
+var DB *gorm.DB
+
+func Connect() {
+	host := os.Getenv("DB_HOST")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	database := os.Getenv("DB_DATABASE")
+	port := os.Getenv("DB_PORT")
+	var err error
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%v sslmode=disable",
+		host,
+		user,
+		password,
+		database,
+		port,
+	)
+
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Info),
+	})
+
+	if err != nil {
+		log.Panicf("failed to connect database,err: %v", err)
+	}
+
+	if err := DB.AutoMigrate(
+		&entity.User{},
+		&entity.Answer{},
+		&entity.Quiz{},
+		&entity.Question{},
+		&entity.Lesson{},
+		&entity.Code{},
+		&entity.CodeAnswer{},
+		&entity.CodeSubmission{},
+		&entity.Choice{},
+		&entity.UserQuiz{},
+		&entity.UserAnswer{},
+		&entity.UserType{},
+	); err != nil {
+		panic("failed to migrate database")
+	}
+
+	fmt.Println("Connection Opened to Database")
+	if err := Seed(); err != nil {
+		fmt.Println("failed to seed database")
+	}
+	
+}
