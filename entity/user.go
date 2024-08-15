4e0f58c5b9c3fe1cbc39b7c6573d9639d70f65ae
Züleyha <yetkinzuleyha94@gmail.com>
2024-08-15 16:26:31 +0300

    user corrected

--- entity/user.go
+++ entity/user.go
@@ -5,14 +5,15 @@ import (
 )
 
 type User struct {
-	ID        int       `json:"id" gorm:"primary key "`
-	FirstName string    `json:"first_name" validation:"required"`
-	LastName  string    `json:"last_name" validation:"required"`
-	Password  string    `json:"password"`
-	Email     string    `json:"email"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
-	DeletedAt time.Time `json:"deleted_at"`
-	UserType  Student   `json:"user_type"`
-	Lesson    string    `json:"lesson"`
+	ID         int       `json:"id" gorm:"primary key "`
+	FirstName  string    `json:"first_name" validation:"required"`
+	LastName   string    `json:"last_name" validation:"required"`
+	Password   string    `json:"password"`
+	Email      string    `json:"email"`
+	CreatedAt  time.Time `json:"created_at"`
+	UpdatedAt  time.Time `json:"updated_at"`
+	DeletedAt  time.Time `json:"deleted_at"`
+	UserType   UserType  `json:"user_type"`
+	UserTypeID int       `json:"user_type_id"`
+	Lessons    []Lesson  `json:"lesson"`
 }
