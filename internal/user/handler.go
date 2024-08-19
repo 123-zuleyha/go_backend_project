29610ed6f05813820e5ec5898766f8a10bc230d6
Züleyha <yetkinzuleyha94@gmail.com>
2024-08-19 12:00:10 +0300

    ..

---
+++ internal/user/handler.go
@@ -0,0 +1,182 @@
+package user
+
+import (
+	"github.com/123-zuleyha/go_backend_project/pkg/utils"
+	"github.com/123-zuleyha/go_backend_project/pkg/utils/models"
+	"github.com/gofiber/fiber/v2"
+)
+
+type UserHandler struct {
+	service UserService
+}
+
+func NewUserHnadler(service *UserService) *UserHandler {
+	return &UserHandler{service: *&service}
+}
+
+func (u *UserHandler) GetUsers(c *fiber.Ctx) error {
+	p := new(models.PaginateRequest)
+	if err := c.QueryParser(p); err != nil {
+		return err
+	}
+
+	users, err := u.service.GetUsers(p)
+	if err != nil {
+		return err
+	}
+	return c.JSON(users)
+
+}
+
+func (u *UserHandler) GetUserByID(c *fiber.Ctx) error {
+	id := c.Params("id")
+
+	user, err := u.service.GetUserByID(utils.StringToInt(id))
+	if err != nil {
+		return err
+	}
+	return c.JSON(user)
+}
+
+func (u *UserHandler) CreateUser(c *fiber.Ctx) error {
+	p := new(CreateUserRequest)
+
+	if err := c.BodyParser(p); err != nil {
+		return err
+	}
+
+	createdUser, err := u.service.CreateUser(p)
+	if err != nil {
+		return err
+	}
+	return c.JSON(createdUser)
+}
+
+func (u *UserHandler) UpdateUser(c *fiber.Ctx) error {
+	p := new(UpdateUserRequest)
+
+	if err := c.BodyParser(p); err != nil {
+		return err
+	}
+	err := u.service.UpdateUser(p)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (u *UserHandler) DeleteUser(c *fiber.Ctx) error {
+	email := c.Params("email")
+
+	user, err := u.service.GetUserByEmail(email)
+	if err != nil {
+
+		return err
+	}
+	return c.JSON(user)
+}
+
+func (u *UserHandler) GetUserByUsername(c *fiber.Ctx) error {
+	username := c.Params("username")
+
+	user, err := u.service.GetUserByUsername(username)
+	if err != nil {
+		return err
+	}
+	return c.JSON(user)
+}
+func (u *UserHandler) SetTeacher(c *fiber.Ctx) error {
+	userID, err := c.ParamsInt("userID")
+	if err != nil {
+		return err
+	}
+
+	return &u.service.SetTeacher(userID)
+}
+
+func (u *UserHandler) GetStudents(c *fiber.Ctx) error {
+	users, err := u.service.GetStudents()
+	if err != nil {
+		return err
+	}
+	return c.JSON(users)
+}
+
+func (u *UserHandler) AddLessonToUser(c *fiber.Ctx) error {
+	userID, err := c.ParamsInt("userID")
+	if err != nil {
+		return err
+	}
+	lessonID, err := c.ParamsInt("lessonID")
+	if err != nil {
+		return err
+	}
+
+	err = u.service.AddLessonToUser(userID, lessonID)
+	if err != nil {
+		return err
+	}
+	return c.JSON(fiber.Map{
+		"message": "Ders kullanıcıya eklendi"})
+}
+
+func (u *UserHandler) RemoveLessonFromUser(c *fiber.Ctx) error {
+	userID, err := c.ParamsInt("userID")
+	if err != nil {
+		return err
+	}
+
+	lessonID, err := c.ParamsInt("lessonID")
+	if err != nil {
+		return err
+	}
+
+	err = u.service.RemoveLessonFromUser(userID, lessonID)
+	if err != nil {
+		return err
+	}
+	return c.JSON(fiber.Map{"message": "Ders kullanıcıdan silindi"})
+
+}
+
+func (u *UserHandler) GetTeacher(c *fiber.Ctx) error {
+	request := new(models.PaginateRequest)
+	if err := c.QueryParser(request); err != nil {
+		return err
+	}
+
+	users, err := u.service.GetTeachers(request)
+	if err != nil {
+		return err
+	}
+	return c.JSON(users)
+}
+
+func (u *UserHandler) GetStudentsByTeacher(c *fiber.Ctx) error {
+	userID, err := c.Params("lessonID")
+	if err != nil {
+		return err
+	}
+	users, err := u.service.GetStudentsByTeacher(userID)
+	if err != nil {
+		return err
+	}
+	return c.JSON(users)
+}
+
+func (u *UserHandler) GetUsersQuizzesByQuizID(c *fiber.Ctx) error {
+	quizID, err := c.ParamsInt("quizID")
+
+	if err != nil {
+		return err
+	}
+	request := new(models.PaginateRequest)
+	if err := c.QueryParser(request); err != nil {
+		return err
+	}
+	users, err := u.service.GetUsersQuizzesByQuizID(quizID, request)
+	if err != nil {
+		return err
+	}
+	return c.JSON(users)
+}
