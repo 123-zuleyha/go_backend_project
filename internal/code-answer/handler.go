29649ab13f5a2f70f8e4c68a52eb0028f47fa593
Züleyha <yetkinzuleyha94@gmail.com>
2024-08-20 00:57:24 +0300

    .

---
+++ internal/code-answer/handler.go
@@ -0,0 +1,90 @@
+package codeanswer
+
+import (
+	"github.com/gofiber/fiber/v2"
+)
+
+type CodeAnswerHandler struct {
+	service CodeAnswerService
+}
+
+func NewCodeAnswerHandler(service *CodeAnswerService) *CodeAnswerHandler {
+	return &CodeAnswerHandler{service: *service}
+}
+
+func (ca *CodeAnswerHandler) GetCodeAnswers(c *fiber.Ctx) error {
+	p := new(BaseRequest)
+	if err := c.QueryParser(p); err != nil {
+		return err
+	}
+	codeAnswers, err := ca.service.GetCodeAnswers(p)
+	if err != nil {
+		return err
+	}
+	return c.JSON(codeAnswers)
+}
+
+func (ca *CodeAnswerHandler) CreateCodeAnswer(c *fiber.Ctx) error {
+	p := new(CreateCodeAnswerRequest)
+	if err := c.BodyParser(p); err != nil {
+		return err
+	}
+	codeAnswers, err := ca.service.CreateCodeAnswers(p)
+	if err != nil {
+		return err
+	}
+	return c.JSON(codeAnswers)
+}
+
+func (ca *CodeAnswerHandler) UpdateCodeAnswer(c *fiber.Ctx) error {
+	p := new(UpdateCodeAnswerRequest)
+	if err := c.BodyParser(p); err != nil {
+		return err
+	}
+	codeAnswers, err := ca.service.UpdateCodeAnswer(p)
+	if err != nil {
+		return err
+	}
+	return c.JSON(codeAnswers)
+}
+
+func (ca *CodeAnswerHandler) DeleteCodeAnswer(c *fiber.Ctx) error {
+	id, err := c.ParamsInt("id")
+	if err != nil {
+		return err
+	}
+	err = ca.service.DeleteCodeAnswer(id)
+	if err != nil {
+		return err
+	}
+	return c.SendStatus(fiber.StatusNoContent)
+}
+
+func (ca *CodeAnswerHandler) GetCodeAnswersByUserID(c *fiber.Ctx) error {
+	p := new(BaseRequest)
+	if err := c.QueryParser(p); err != nil {
+		return err
+	}
+	userID := c.Locals("user_id").(int)
+	codeAnswers, err := ca.service.GetCodeAnswersByUserID(p, userID)
+	if err != nil {
+		return err
+	}
+	return c.JSON(codeAnswers)
+}
+
+func (ca *CodeAnswerHandler) GetCodeAnswerByID(c *fiber.Ctx) error {
+	p := new(BaseRequest)
+	if err := c.QueryParser(p); err != nil {
+		return err
+	}
+	id, err := c.ParamsInt("id")
+	if err != nil {
+		return err
+	}
+	codeAnswer, err := ca.service.GetCodeAnswerByID(id)
+	if err != nil {
+		return err
+	}
+	return c.JSON(codeAnswer)
+}
---
+++ internal/code-answer/service.go
@@ -0,0 +1,96 @@
+package codeanswer
+
+import (
+	"errors"
+
+	"github.com/123-zuleyha/go_backend_project/db/entity"
+	"github.com/123-zuleyha/go_backend_project/pkg/utils"
+)
+
+type CodeAnswerService struct {
+	repository CodeAnswerRepository
+}
+
+func NewCodeAnswerService(repository *CodeAnswerRepository) *CodeAnswerService {
+	return &CodeAnswerService{repository: *repository}
+}
+
+func (s *CodeAnswerService) GetCodeAnswers(req *BaseRequest) (*CodeAnswerResponseDTO, error) {
+	codeAnswers, err := s.repository.GetCodeAnswers(req)
+	if err != nil {
+		return nil, err
+	}
+	codeAnswerDTOs := []CodeAnswerDTO{}
+	for i := range codeAnswers {
+		codeAnswerDTO := new(CodeAnswerDTO)
+		err := utils.JSONtoDTO(codeAnswers[i], codeAnswerDTO)
+
+		if err != nil {
+			return nil, errors.New("failed to convert code answer entity to code answer dto")
+		}
+		codeAnswerDTOs = append(codeAnswerDTOs, *codeAnswerDTO)
+	}
+
+	var resultDTO CodeAnswerResponseDTO
+	resultDTO.Count = len(codeAnswerDTOs)
+	resultDTO.Data = codeAnswerDTOs
+
+	return &resultDTO, nil
+}
+
+func (s *CodeAnswerService) CreateCodeAnswers(codeAnswerDTO *CreateCodeAnswerRequest) (*entity.CodeAnswer, error) {
+	codeAnswerEntity := new(entity.CodeAnswer)
+	utils.DTOtoJSON(codeAnswerDTO, codeAnswerEntity)
+
+	createdCodeAnswer, err := s.repository.CreateCodeAnswer(codeAnswerEntity)
+	if err != nil {
+		return nil, err
+	}
+	return createdCodeAnswer, nil
+}
+
+func (s *CodeAnswerService) UpdateCodeAnswer(codeAnswerDTO *UpdateCodeAnswerRequest) (*entity.CodeAnswer, error) {
+	codeAnswerEntity := new(entity.CodeAnswer)
+	if err := utils.DTOtoJSON(codeAnswerDTO, codeAnswerEntity); err != nil {
+		return nil, err
+	}
+	err := s.repository.UpdateCodeAnswer(*codeAnswerEntity)
+	if err != nil {
+		return nil, err
+	}
+	return codeAnswerEntity, nil
+}
+
+func (s *CodeAnswerService) DeleteCodeAnswer(id int) error {
+	return s.repository.DeleteCodeAnswer(id)
+}
+
+func (s *CodeAnswerService) GetCodeAnswersByUserID(req *BaseRequest, userID int) (*CodeAnswerResponseDTO, error) {
+	codeAnswers, err := s.repository.GetCodeAnswersByUserID(req, userID)
+	if err != nil {
+		return nil, err
+	}
+	codeAnswerDTOs := []CodeAnswerDTO{}
+	for i := range codeAnswers {
+		codeAnswerDTO := new(CodeAnswerDTO)
+		err := utils.JSONtoDTO(codeAnswers[i], codeAnswerDTO)
+		if err != nil {
+			return nil, errors.New("failed to convert code answer entity to code answer dto")
+		}
+		codeAnswerDTOs = append(codeAnswerDTOs, *codeAnswerDTO)
+	}
+	var resultDTO CodeAnswerResponseDTO
+	resultDTO.Count = len(codeAnswerDTOs)
+	resultDTO.Data = codeAnswerDTOs
+	return &resultDTO, nil
+}
+
+func (s *CodeAnswerService) GetCodeAnswerByID(id int) (*CodeAnswerDTO, error) {
+	codeAnswer, err := s.repository.GetCodeAnswerByID(id)
+	if err != nil {
+		return nil, err
+	}
+	codeAnswerDTO := new(CodeAnswerDTO)
+	utils.JSONtoDTO(codeAnswer, codeAnswerDTO)
+	return codeAnswerDTO, nil
+}
