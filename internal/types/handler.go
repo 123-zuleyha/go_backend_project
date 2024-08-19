3b9b6d9adda5e35d980d7a243b360247aad5e8e5
Züleyha <yetkinzuleyha94@gmail.com>
2024-08-19 14:53:26 +0300

    handler

---
+++ internal/types/handler.go
@@ -0,0 +1,36 @@
+package types
+
+import "github.com/gofiber/fiber/v2"
+
+type UserTypeHandler struct {
+	service UserTypeService
+}
+
+func NewUserTypeHandler(service *UserTypeService) *UserTypeHandler {
+	return &UserTypeHandler{service: *service}
+}
+
+func (u *UserTypeHandler) GetUserTypes(c *fiber.Ctx) error {
+	p := new(BaseRequest)
+	if err := c.QueryParser(p); err != nil {
+		return err
+	}
+
+	userTypes, err := u.service.GetUserTypes(p)
+	if err != nil {
+		return err
+	}
+	return c.JSON(userTypes)
+}
+
+func (u *UserTypeHandler) CreateUserType(c *fiber.Ctx) error {
+	p := new(CreateUserTypeRequest)
+	if err := c.BodyParser(p); err != nil {
+		return err
+	}
+	userTypes, err := u.service.CreateUserType(p)
+	if err != nil {
+		return err
+	}
+	return c.JSON(userTypes)
+}
