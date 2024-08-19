771412a547c23d3254555cf8a3d59773bb724a02
Züleyha <yetkinzuleyha94@gmail.com>
2024-08-19 12:32:11 +0300

    mapper

--- internal/user/mapper.go
+++ internal/user/mapper.go
@@ -1,41 +1,41 @@
 package user
 
-import {
+import (
 	"time"
-}
 
+	"github.com/123-zuleyha/go_backend_project/pkg/lesson"
+)
 
-type PaginatedUserResponse struct  {
-	Count int `json:"count"`
-	Data []UserDTO `json:"data"`
+type PaginatedUserResponse struct {
+	Count int       `json:"count"`
+	Data  []UserDTO `json:"data"`
 }
 
 type UserDTO struct {
-	ID           int                    `json:"id"`
-	FirstName    string           `json:"firstname"`
-	LastName     string            `json:"lastname"`
-	Username     string            `json:"username"`
-	Email        string                `json:"email"`
-	UserType     *UserTypeDTO       `json:"user_types"`
-	CreatedAt    time.Time         `json:"createdat"`
-	UpdatedAt    time.Time          `json:"updatedat"`
-
-	Lessons      []lesson.LessonDTO    `json:"lessons"`
+	ID        int                `json:"id"`
+	FirstName string             `json:"firstname"`
+	LastName  string             `json:"lastname"`
+	Username  string             `json:"username"`
+	Email     string             `json:"email"`
+	UserType  *UserTypeDTO       `json:"user_types"`
+	CreatedAt time.Time          `json:"createdat"`
+	UpdatedAt time.Time          `json:"updatedat"`
+	Lessons   []lesson.LessonDTO `json:"lessons"`
 }
 
 type UserTypeDTO struct {
-	ID int `json:"id"`
-	Name string  `json:"name"`
+	ID   int    `json:"id"`
+	Name string `json:"name"`
 }
 
 type CreateUserRequest struct {
-	FirstName  string  `json:"firstname"`
-	LastName string    `json:"lastname"`
-	Password string     `json:"password"`
-	Email string      `json:"email"`
+	FirstName string `json:"firstname"`
+	LastName  string `json:"lastname"`
+	Password  string `json:"password"`
+	Email     string `json:"email"`
 }
 
 type UpdateUserRequest struct {
-	Password string   `json:"password"`
-	Email     string   `json:"email"`
+	Password string `json:"password"`
+	Email    string `json:"email"`
 }
