4858b8038409068fc00db8fab5a5784d8cd08e55
Züleyha <yetkinzuleyha94@gmail.com>
2024-08-25 13:42:56 +0300

    db.go

--- db/db.go
+++ db/db.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"os"
 
+	"github.com/123-zuleyha/go_backend_project/db/entity"
 	"github.com/123-zuleyha/go_backend_project/db/seeders"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -20,32 +21,50 @@ func Connect() {
 	password := os.Getenv("DB_PASSWORD")
 	database := os.Getenv("DB_DATABASE")
 	port := os.Getenv("DB_PORT")
-
-	if host == "" || user == "" || password == "" || database == "" || port == "" {
-		log.Fatalf("One or more database environment variables are not set. Please check DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE, and DB_PORT.")
-	}
-
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s", host, user, password, database, port)
-
 	var err error
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%v sslmode=disable",
+		host,
+		user,
+		password,
+		database,
+		port,
+	)
+
 	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{
 		Logger: logger.Default.LogMode(logger.Info),
 	})
+
 	if err != nil {
-		log.Fatalf("Failed to connect database: %v", err)
+		log.Panicf("failed to connect database,err: %v", err)
 	}
 
-	// Modeller buraya eklenecek :)
-	if err := DB.AutoMigrate(); err != nil {
-		panic("Failed to migrate database")
+	if err := DB.AutoMigrate(
+		&entity.User{},
+		&entity.Answer{},
+		&entity.Quiz{},
+		&entity.Question{},
+		&entity.Lesson{},
+		&entity.Code{},
+		&entity.CodeAnswer{},
+		&entity.CodeSubmission{},
+		&entity.Choice{},
+		&entity.UserQuiz{},
+		&entity.UserAnswer{},
+		&entity.UserType{},
+	); err != nil {
+		panic("failed to migrate database")
 	}
-	fmt.Println("Connection to database established successfully")
+
+	fmt.Println("Connection Opened to Database")
+	if err := Seed(); err != nil {
+		fmt.Println("failed to seed database")
+	}
+	fmt.Println("Database Seeded")
 }
 
 func Seed() error {
 	userTypeSeeder := seeders.UserTypeSeeder{}
 	userSeeder := seeders.UserSeeder{}
-
 	err := DB.Clauses(clause.OnConflict{
 		Columns:   []clause.Column{{Name: "id"}},
 		UpdateAll: true,
